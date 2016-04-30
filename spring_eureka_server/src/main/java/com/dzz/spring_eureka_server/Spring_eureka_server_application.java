ac6effd2796b722009f1376a7ba0dcb4fbe6c1af
zori0 <776021342@qq.com>
2016-04-30 18:32:58 +0800

    修正文件

--- spring_eureka_server/src/main/java/com/dzz/spring_eureka_server/Spring_eureka_server_application.java
+++ spring_eureka_server/src/main/java/com/dzz/spring_eureka_server/Spring_eureka_server_application.java
@@ -1,7 +1,6 @@
 package com.dzz.spring_eureka_server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 /**
