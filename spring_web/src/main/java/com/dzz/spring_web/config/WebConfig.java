fa79b9633b8cccc724843608dda4fdc2f69e5f17
zori0 <776021342@qq.com>
2016-04-30 10:56:10 +0800

    api定义

---
+++ spring_web/src/main/java/com/dzz/spring_web/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.dzz.spring_web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by dzz on 16/4/30.
+ */
+@EnableAutoConfiguration
+public class WebConfig {
+
+
+}
--- spring_web/src/main/java/com/dzz/spring_web/controller/WelcomeController.java
+++ spring_web/src/main/java/com/dzz/spring_web/controller/WelcomeController.java
@@ -1,18 +1,26 @@
 package com.dzz.spring_web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
 
 /**
  * Created by dzz on 16/4/30.
  */
 
 @Controller
+@EnableAutoConfiguration
 public class WelcomeController {
 
-    @RequestMapping(value = "/")
-    public String welcome(){
+    @Value("${user.name}")
+    private  String name;
 
+    @RequestMapping(value = "/")
+    public String welcome(ModelMap map){
+
+        map.put("name",name);
         return "welcome";
     }
 }
