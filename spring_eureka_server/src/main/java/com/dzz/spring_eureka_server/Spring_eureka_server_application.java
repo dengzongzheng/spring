04e86b20b7557809106d31b24526e559dc07659a
zori0 <776021342@qq.com>
2016-04-30 16:40:23 +0800

    添加服务发现

---
+++ spring_eureka_server/src/main/java/com/dzz/spring_eureka_server/Spring_eureka_server_application.java
@@ -0,0 +1,20 @@
+package com.dzz.spring_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by dzz on 16/4/30.
+ */
+@SpringBootApplication
+@Configuration
+@EnableEurekaServer
+public class Spring_eureka_server_application {
+
+    public static void main(String args[]){
+
+        SpringApplication.run(Spring_eureka_server_application.class, args);
+    }
+
+}
